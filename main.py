4cadf75a58a029ba3d44ddc72c0d4234927071af
Rakesh Gogineni <rg793@njit.edu>
2024-12-20 21:39:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-# Pydantic models
+
 class Customer(BaseModel):
     customer_id: Optional[int] = None
     name: str
